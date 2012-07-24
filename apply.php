89c6f39d1d7fec896e9f68133fc57841e28aac86
jesalg <jesal7@gmail.com>
2012-07-24 16:42:15 -0700

    Client side validation

--- apply.php
+++ apply.php
@@ -1,12 +1,12 @@
 <?php
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['Submit'])) {
 
-    if ($_POST['name'] != "") {
-        $_POST['name'] = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-        if ($_POST['name'] == "") {
-            $errors .= 'Please enter a valid name.<br/><br/>';
-        }
+    $errors = null;
+
+    if (empty($_POST['first_name']) || empty($_POST['last_name'])) {
+        $_POST['first_name'] = filter_var($_POST['first_name'], FILTER_SANITIZE_STRING);
+        $_POST['last_name'] = filter_var($_POST['last_name'], FILTER_SANITIZE_STRING);
     } else {
         $errors .= 'Please enter your name.<br/>';
     }
@@ -20,31 +20,11 @@ if (isset($_POST['submit'])) {
         $errors .= 'Please enter your email address.<br/>';
     }
 
-    if ($_POST['homepage'] != "") {
-        $homepage = filter_var($_POST['homepage'], FILTER_SANITIZE_URL);
-        if (!filter_var($homepage, FILTER_VALIDATE_URL)) {
-            $errors .= "$homepage is <strong>NOT</strong> a valid URL.<br/><br/>";
-        }
-    } else {
-        $errors .= 'Please enter your home page.<br/>';
-    }
-
-    if ($_POST['message'] != "") {
-        $_POST['message'] = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-        if ($_POST['message'] == "") {
-            $errors .= 'Please enter a message to send.<br/>';
-        }
-    } else {
-        $errors .= 'Please enter a message to send.<br/>';
-    }
-
     if (!$errors) {
         $mail_to = 'jesal@calistolabs.com';
         $subject = 'New Career Form Submission';
         $message  = 'From: ' . $_POST['name'] . "\n";
         $message .= 'Email: ' . $_POST['email'] . "\n";
-        $message .= 'Homepage: ' . $_POST['homepage'] . "\n";
-        $message .= "Message:\n" . $_POST['message'] . "\n\n";
         mail($to, $subject, $message);
 
         echo "Thank you for your email!<br/><br/>";
@@ -52,4 +32,5 @@ if (isset($_POST['submit'])) {
         echo '<div style="color: red">' . $errors . '<br/></div>';
     }
 }
+
 ?>
