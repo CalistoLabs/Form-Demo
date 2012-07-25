5896e7888c3ecc452a7774b27e7aa015a2e9568d
jesalg <jesal7@gmail.com>
2012-07-24 18:17:45 -0700

    Preliminary file upload logic

--- apply.php
+++ apply.php
@@ -2,16 +2,41 @@
 
 if (isset($_POST['Submit'])) {
 
+    $allowed_filetypes = array('.doc','.docx','.pdf');
+    $max_filesize = 10485760;
+    $upload_path = './uploads/resume/';
     $errors = null;
 
-    if (empty($_POST['first_name']) || empty($_POST['last_name'])) {
-        $_POST['first_name'] = filter_var($_POST['first_name'], FILTER_SANITIZE_STRING);
-        $_POST['last_name'] = filter_var($_POST['last_name'], FILTER_SANITIZE_STRING);
+    if(isset($_FILES['resume'])) {
+    $filename = $_FILES['resume']['name'];
+    $ext = strrchr($filename,'.');
+
+    // Check if the filetype is allowed, if not DIE and inform the user.
+    if(!in_array($ext,$allowed_filetypes))
+        $errors .= 'The file you attempted to upload is not allowed.';
+
+    // Now check the filesize, if it is too large then DIE and inform the user.
+    if(filesize($_FILES['resume']['tmp_name']) > $max_filesize)
+        $errors .= 'The file you attempted to upload is too large.';
+
+    // Check if we can upload to the specified path, if not DIE and inform the user.
+    if(!is_writable($upload_path))
+        $errors .= 'You cannot upload to the specified directory, please CHMOD it to 777.';
+
+    // Upload the file to your specified path.
+    if(move_uploaded_file($_FILES['resume']['tmp_name'],$upload_path . $filename))
+        $resume = $upload_path . $filename;
+    else
+        $errors .= 'There was an error during the file upload.  Please try again.';
+    }
+
+    if (!empty($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     } else {
         $errors .= 'Please enter your name.<br/>';
     }
 
-    if ($_POST['email'] != "") {
+    if (!empty($_POST['email'])) {
         $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $errors .= "$email is <strong>NOT</strong> a valid email address.<br/><br/>";
@@ -21,10 +46,10 @@ if (isset($_POST['Submit'])) {
     }
 
     if (!$errors) {
-        $mail_to = 'jesal@calistolabs.com';
+        $to = 'jesal@calistolabs.com';
         $subject = 'New Career Form Submission';
-        $message  = 'From: ' . $_POST['name'] . "\n";
-        $message .= 'Email: ' . $_POST['email'] . "\n";
+        $message  = 'From: ' . $name . "\n";
+        $message .= 'Email: ' . $email . "\n";
         mail($to, $subject, $message);
 
         echo "Thank you for your email!<br/><br/>";
